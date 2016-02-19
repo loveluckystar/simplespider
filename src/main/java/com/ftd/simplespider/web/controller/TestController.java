19a31bd4111de25f6fed8f28072ba6c6b3c2ef7b
loveluckystar <lixing89@sina.com>
2016-02-19 17:53:00 +0800

    提交代码

---
+++ src/main/java/com/ftd/simplespider/web/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.ftd.simplespider.web.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @ClassName: TestController 
+ * @author bjlixing 
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	@RequestMapping("t!test.action")
+	public String test(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("in");
+		try {
+			return "/template/test";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+}
